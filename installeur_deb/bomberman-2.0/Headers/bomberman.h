366759b160747233064be477a40638d581416910
Nada MESRATI <mesrat_n@etna-alternance.net>
2019-02-12 19:39:51 +0100

    installeur debian fait, a tester sur une vm vierge

---
+++ installeur_deb/bomberman-2.0/Headers/bomberman.h
@@ -0,0 +1,79 @@
+#ifndef BOMBERMAN_H_
+#define BOMBERMAN_H_
+
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <errno.h>
+#include <unistd.h>
+
+#define MAX_PLAYERS 4
+#define MAP_COL 45 /* y | */
+#define MAP_ROW 45 /* x --- */
+#define MAP_SIZE (MAP_COL * MAP_ROW)
+#define TILE_HEIGHT 16 /* y */
+#define TILE_WIDTH 16 /* x */
+#define BOMB_TICKS 1500
+#define ERR_MSG(...) { fprintf(stderr, __VA_ARGS__); exit(1); }
+
+#pragma pack(2)
+
+typedef struct s_bomb t_bomb;
+
+typedef struct s_bomb
+{
+        double  ticks_left;
+        int y;
+        int x;
+        t_bomb* prev;
+        t_bomb* next;
+}s_bomb;
+
+typedef struct s_player_info
+{
+        char  connected;
+        char  alive;
+        int   x_pos;
+        int   y_pos;
+        int   bombs_left;
+}               t_player_info;
+
+typedef struct  s_client_request
+{
+        int           x_pos;                  /* La position x souhait?e par le client */
+        int           y_pos;                  /* La position y souhait?e par le client */
+        int           command;                /* Une commande du client (0 : Ne rien faire / 1 : Poser une bombe) */
+}t_client_request;
+
+typedef char t_map[MAP_SIZE];
+
+typedef struct  s_game
+{
+        t_player_info           players[MAX_PLAYERS];
+        t_map                   map;
+        t_bomb*                 bomb;
+}t_game;
+
+typedef struct s_server {
+        pthread_t               tid;
+        t_game                  game;
+        int                     fds[MAX_PLAYERS];
+        struct sockaddr_in      sock_serv;
+        struct sockaddr         *sock_ptr;
+        socklen_t               len;
+        int                     sockfd;
+        int                     port;
+        int                     running;
+} t_server;
+
+
+void    game_process(t_server*, t_client_request*, int userIndex);
+void    game_tick(t_game*);
+void    game_init_players(t_game*);
+void    bomb_things(t_game* game);
+int     server(int host);
+
+#endif
---
+++ installeur_deb/bomberman_2.0-1.dsc
@@ -0,0 +1,19 @@
+Format: 3.0 (quilt)
+Source: bomberman
+Binary: bomberman
+Architecture: any
+Version: 2.0-1
+Maintainer: Nada MESRATI <mesrat_n@etna-alternance.net>
+Standards-Version: 3.9.2
+Build-Depends: debhelper (>= 9), libsdl2-image-dev, libsdl2-2.0-0, libsdl2-image-2.0-0, libsdl2-mixer-2.0-0
+Package-List:
+ bomberman deb misc optional arch=any
+Checksums-Sha1:
+ b6e4eac9dc33205695c54df9e1d8b4fa75b71a05 38728 bomberman_2.0.orig.tar.gz
+ 3034eb41c814e4e04acc0270036681a74f2d9df6 856 bomberman_2.0-1.debian.tar.xz
+Checksums-Sha256:
+ 9e6d53ff5767b15434b438361617a742c8d87ab911ea94bef068ccd863e8040d 38728 bomberman_2.0.orig.tar.gz
+ e50a53f68d1923119140aef0f9c60580b3595561753f4660c7bb5f2705afcace 856 bomberman_2.0-1.debian.tar.xz
+Files:
+ f9d4182a7c0e82e1176f7481edeb96b7 38728 bomberman_2.0.orig.tar.gz
+ f2cbb53b631f44886110a02c9ff2f312 856 bomberman_2.0-1.debian.tar.xz
